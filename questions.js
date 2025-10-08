4ffb858ae6ae6f1b97c93ab1008d41ca9e32a3d0
Luffy-Montoya <80363512+Luffy-Montoya@users.noreply.github.com>
2025-10-07 22:13:05 -0700

    Add files via upload

--- questions.js
+++ questions.js
@@ -1,80 +1,79 @@
 const head = {
-    any: "Were they wearing ANYTHING on their head?",
-    glasses: "Were they wearing GLASSES?",
-    hat: "Were they wearing a HAT?",
-    bowBand: "Were they wearing a BOW or HEADBAND?"
+    any: "Do they have ANYTHING on their head?",
+    glasses: "Do they wear GLASSES?",
+    hat: "Are they wearing a HAT?",
+    bowBand: "Are they wearing a BOW or HEADBAND?"
 }
 
 const hair = {
-    black: "Did they have BLACK hair?",
-    brown: "Did they have BROWN hair?",
-    blonde: "Did they have BLONDE hair?",
-    orange: "Did they have ORANGE hair?",
-    gray: "Did they have GRAY hair?"
+    black: "Do they have BLACK hair?",
+    brown: "Do they have BROWN hair?",
+    blonde: "Do they have BLONDE hair?",
+    orange: "Do they have ORANGE hair?",
+    gray: "Do they have GRAY hair?"
 }
 
 const genderSkin = {
-    man: "Were they a MAN?",
-    woman: "Were they a WOMAN?",
-    light: "Did they have LIGHT skin?",
-    dark: "Did they have DARK skin?"
+    man: "Are they a MAN?",
+    woman: "Are they a WOMAN?",
+    light: "Do they have LIGHT skin?",
+    dark: "Do they have DARK skin?"
 }
 
 const clothes = {
-    jacket: "Were they wearing a JACKET?",
-    suit: "Were they wearing a SUIT?",
-    tie: "Were they wearing a TIE?",
-    apron: "Were they wearing an APRON?",
-    skirt: "Were they wearing a SKIRT?",
-    pants: "Were they wearing PANTS?"
+    jacket: "Are they wearing a JACKET?",
+    suit: "Are they wearing a SUIT?",
+    tie: "Are they wearing a TIE?",
+    apron: "Are they wearing an APRON?",
+    skirt: "Are they wearing a SKIRT?",
+    pants: "Are they wearing PANTS?"
 }
 
 const shirt = {
-    red: "Were they wearing a RED shirt?",
-    blue: "Were they wearing a BLUE shirt?",
-    green: "Were they wearing a GREEN shirt?",
-    purple: "Were they wearing a PURPLE shirt?",
-    pink: "Were they wearing a PINK shirt?",
-    white: "Were they wearing a WHITE shirt?"
+    red: "Are they wearing a RED shirt?",
+    blue: "Are they wearing a BLUE shirt?",
+    green: "Are they wearing a GREEN shirt?",
+    purple: "Are they wearing a PURPLE shirt?",
+    pink: "Are they wearing a PINK shirt?",
+    white: "Are they wearing a WHITE shirt?"
 }
 
 const pants = {
-    any: "Were they wearing ANY pants at all?",
-    blue: "Were they wearing BLUE pants?",
-    gray: "Were they wearing GRAY pants?",
-    black: "Were they wearing BLACK pants?",
-    red: "Were they wearing RED pants?",
-    brown: "Were they wearing BROWN pants?",
-    green: "Were they wearing GREEN pants?",
-    magenta: "Were they wearing MAGENTA pants?"
+    any: "Are they wearing ANY pants at all?",
+    blue: "Are they wearing BLUE pants?",
+    gray: "Are they wearing GRAY pants?",
+    black: "Are they wearing BLACK pants?",
+    red: "Are they wearing RED pants?",
+    brown: "Are they wearing BROWN pants?",
+    green: "Are they wearing GREEN pants?",
+    magenta: "Are they wearing MAGENTA pants?"
 }
 
 const shoes = {
-    black: "Were they wearing BLACK shoes?",
-    white: "Were they wearing WHITE shoes?",
-    gray: "Were they wearing GRAY shoes?",
-    brown: "Were they wearing BROWN shoes?",
-    blue: "Were they wearing BLUE shoes?",
-    red: "Were they wearing RED shoes?",
-    purple: "Were they wearing PURPLE shoes?"
+    black: "Are they wearing BLACK shoes?",
+    white: "Are they wearing WHITE shoes?",
+    gray: "Are they wearing GRAY shoes?",
+    brown: "Are they wearing BROWN shoes?",
+    blue: "Are they wearing BLUE shoes?",
+    red: "Are they wearing RED shoes?",
+    purple: "Are they wearing PURPLE shoes?"
 }
 
 const acc = {
-    any: "Did they have ANY accessories at all?",
-    whistle: "Were they wearing a WHISTLE?",
-    books: "Were they carrying BOOKS?",
-    clipboard: "Were they holding a CLIPBOARD?",
-    purse: "Did they have a PURSE?",
-    badge: "Were they wearing a BADGE?",
-    math: "Were they holding a bunch of MATH STUFF?",
-    broom: "Did he have a BROOM?",
-    animals: "Were they carrying any ANIMALS?",
-    mask: "Did they have a MASK?",
-    basketball: "Did they have a BASKETBALL?",
-    computer: "Were they carrying a COMPUTER?",
-    microphone: "Did they have a magical floating MICROPHONE?",
-    pad: "Were they carrying a WRITING PAD?",
-    science: "Were they holding up a bunch of SCIENCE STUFF?"
+    any: "Do they have ANY accessories at all?",
+    whistle: "Are they wearing a WHISTLE?",
+    books: "Are they carrying BOOKS?",
+    clipboard: "Are they holding a CLIPBOARD?",
+    purse: "Do they have a PURSE?",
+    badge: "Are they wearing a BADGE?",
+    math: "Are they holding a bunch of MATH STUFF?",
+    broom: "Does he have a BROOM?",
+    animals: "Are they carrying any ANIMALS?",
+    mask: "Do they have a MASK?",
+    basketball: "Do they have a BASKETBALL?",
+    computer: "Are they carrying a COMPUTER?",
+    pad: "Are they carrying a WRITING PAD?",
+    science: "Are they holding a bunch of SCIENCE STUFF?"
 }
 
 export {
