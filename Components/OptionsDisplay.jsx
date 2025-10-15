789aa44b7f8a49a499f4038e0c4f3732fabb4d99
Luffy-Montoya <80363512+Luffy-Montoya@users.noreply.github.com>
2025-10-15 05:47:15 -0700

    Add files via upload

--- Components/OptionsDisplay.jsx
+++ Components/OptionsDisplay.jsx
@@ -2,8 +2,11 @@ import React from "react"
 
 export default function OptionsDisplay({ children }) {
     return(
-        <div className="options-display">
-            {children}
-        </div>
+        <>
+            <div className="options-display">
+                {children}
+            </div>
+            <div className="options-ghost"></div>
+        </>
     )
 }
