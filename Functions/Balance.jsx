63538931612076968233e35b3ffa1db0610fffa8
Luffy-Montoya <80363512+Luffy-Montoya@users.noreply.github.com>
2025-10-26 02:46:16 -0700

    Add files via upload

--- Functions/Balance.jsx
+++ Functions/Balance.jsx
@@ -10,11 +10,11 @@ const insightValue = 12
 
 const charityChance = [0, 8, 14, 20, 25]
 
-const luckyValue = 4
+const luckyValue = 6
 const luckyInc = 1
 const luckyRate = 10
 
-const unluckyValue = 2
+const unluckyValue = 3
 const unluckyInc = 1
 const unluckyRate = 20
 
